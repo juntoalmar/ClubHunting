97366878b3e8dde93ed559f9b3fbcde9010ac1d1
Edu Garcia <profegs@yahoo.es>
2018-11-26 09:27:16 +0100

    Original Commit

--- app/src/main/java/se/kth/mobdev/clubhunting/MapsActivity.java
+++ app/src/main/java/se/kth/mobdev/clubhunting/MapsActivity.java
@@ -24,7 +24,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_button);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used. blah
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used. blah2
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
